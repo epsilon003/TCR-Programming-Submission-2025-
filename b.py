9d8b4c5bf4b26f96536f2ab2bd51fce52d8c8862
Abhimantr Singh <abhimantrsingh@gmail.com>
2025-04-13 22:17:44 +0530

    Add files via upload

---
+++ b.py
@@ -0,0 +1,22 @@
+from c import random_noise
+
+def star(String):
+    return "--*--" + String + "--*--"
+
+def wowify(String):
+    return "✨" + String.upper() + "✨"
+
+def invert_case(String):
+    return ''.join(character.lower() if character.isupper() else character.upper() for character in String)
+
+def add_noise(String):
+    if not isinstance(random_noise(), str):
+        raise ValueError("random_noise must be a string")
+    return random_noise() + String + random_noise()
+
+def echo(String):
+    return (String + " ") * 5 
+
+def wrap_in_brackets(String):
+    return "[[" + String + "]]"
+ 
